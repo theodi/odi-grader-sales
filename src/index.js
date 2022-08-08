749443281a71561d94e78d1f3733d1668e89aaad
jargonautical <lucy@jargonautical.com>
2022-08-08 14:28:30 +0100

    making sure the latest is there

--- src/index.js
+++ src/index.js
@@ -1,20 +1,35 @@
 import CsvToJson from "csvtojson";
 
-const Ajv = require("ajv/dist/jtd");
-const ajv = new Ajv(); // options can be passed, e.g. {allErrors: true}
+const Ajv2020 = require("ajv/dist/2020");
 
+function convertIntObj(input) {
+      const res = {}
+      for (const key in input) {
+        res[key] = {};
+        for (const prop in input[key]) {
+          const parsed = parseInt(input[key][prop], 10);
+          res[key][prop] = isNaN(parsed) ? input[key][prop] : parsed;
+        }
+      }
+      return res;
+    }
 
+const ajv = new Ajv2020({allErrors:true, strict:true});
 const schema = {
+  type: "object",
   properties: {
     OrderID: {type: "string"},
     Booker: {type: "string"},
-    Value: {type: "float32"},
-    Rating: {type: "int32"}
+    Value: {type: "number"},
+    Rating: {
+      type: "number",
+      enum: ["0","1","2","3","4","5"]
+    }
   },
   additionalProperties: true
 };
 
-const serialize = ajv.compileSerializer(schema);
+//const serialize = ajv.compileSerializer(schema);
 
 // fetch data
 let tableCols = [];
@@ -34,16 +49,16 @@ document.getElementById("import").onclick = function () {
       .fromString(e.target.result)
       .then((arr) => {
         let input = arr;
-        console.log("array contents: ", arr); // if not ok, no values
-        console.log("array contents 0: ", arr[0]);
-
+        var result = convertIntObj(input);
+        var arrayResult = Object.values(result);
+        console.log("arrayResult: ", arrayResult);
+        //console.log("array contents: ", arr); // if not ok, no values
         var col = [];
         var record = [];
         var tableData = [];
         var tableHeader = [];
-
-        for (var i = 0; i < input.length; i++) {
-          var record = input[i];
+        for (var i = 0; i < arr.length; i++) {
+          var record = arr[i];
           var recordVals = [];
           var numCols = Object.keys(record).length;
           for (var j = 0; j < numCols; j++) {
@@ -52,30 +67,46 @@ document.getElementById("import").onclick = function () {
             recordVals.push(value);
           }
           tableData.push(recordVals);
-
         }
-        console.log('table data', tableData);
-        console.log(arr.length);
+        //console.log('table data', tableData);
+        //console.log(arr.length);
+        const errorList = [];
         for (let i = 0; i < arr.length; i++) {
           const validate = ajv.compile(schema)
-          //const data = {foo: 1, bar: "abc"}
           const valid = validate(arr[i]);
-          if (!valid) console.log(validate.errors);
-          let msgs = document.getElementById("invalidMessages");
-          let colName = validate.errors[0]["instancePath"].slice(1,);
-          let colType = validate.errors[0]["keyword"];
-          let problem = validate.errors[0]["message"];
-          if (colType === "type") {
-            msgs.textContent = `"${colName}" ${colType} ${problem}.`;
-          } else if (validate.errors[0]["params"]["error"] === "missing") {
-            let missingCol = validate.errors[0]["params"]["missingProperty"];
-            msgs.textContent = `Cannot find required property "${missingCol}".`
+          if (!valid) {
+            //console.log(validate.errors);
+            errorList.push(validate.errors);
           }
-
-
+          let msgs = document.getElementById("invalidMessages");
+          let success = document.getElementById("validFile");
+          //if (valid) console.log("success!");
+          let errs = errorList[0];
+          let errText = [];
+          for (var k = 0; k < errs.length; k++) {
+            let colName = errs[k]["instancePath"].slice(1,);
+            let colType = errs[k]["keyword"];
+            let problem = errs[k]["message"];
+            if (colType === "type") {
+              var errMsg = `"${colName}" ${colType} ${problem}.`;
+              errText.push(errMsg);
+            } else if (errs[k]["params"]["error"] === "missing") {
+              let missingCol = errs[k]["params"]["missingProperty"];
+              var errMsg = `Cannot find required property "${missingCol}".`;
+              errText.push(errMsg);
+            } else if (colType === "enum") {
+              let allowed = errs[k]["params"]["allowedValues"];
+              let missingCol = errs[k]["params"]["missingProperty"];
+              var errMsg = `"${colName}" must be one of ${allowed}.`;
+              errText.push(errMsg);
+            }
+          }
+          function removeDuplicates(errText) {
+            return [...new Set(errText)];
+          }
+          //console.log("errText cleaned: ", removeDuplicates(errText));
+          msgs.innerHTML = `Issues:<br>${removeDuplicates(errText)}`
         }
-
-
         for (var i = 0; i < input.length; i++) {
           for (var key in input[i]) {
             if (col.indexOf(key) === -1) {
@@ -95,10 +126,4 @@ document.getElementById("import").onclick = function () {
         });
       })}
     let myData = fr.readAsText(files.item(0));
-
-
-
-
-
-
   };
